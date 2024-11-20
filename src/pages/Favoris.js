f64514d609be6e2c2a6feb2002afcf3565fbcbd7
LucasDeleplace <Lucasdele59@gmail.com>
2024-11-20 14:13:17 +0100

    add: back previous page in favorite page

--- src/pages/Favoris.js
+++ src/pages/Favoris.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Header from "../components/Header";
 
 const Favoris = () => {
+    const handleClick = () => {
+        window.history.back();
+    }
     return (
         <div>
+            <Header />
             <h1>Mes favoris ★</h1>
+            <p onClick={handleClick}>🔙</p>
         </div>
     )
 }
